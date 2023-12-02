2e291ee3045b2f9fd4b8db1697dec8bc7fa21f91
PhcdY <phucb2411@gmail.com>
2023-12-02 15:32:19 +0700

    upadate memaybeo
    phuc trong cung dz phet

--- Muffin.cpp
+++ Muffin.cpp
@@ -13,16 +13,6 @@ int main()
 
 void MedianAge()
 {
-    int NumberOfPeople;
-    int TotalAge = 0;
-    int Age;
-    cout << "Enter the number of people: ";
-    cin >> NumberOfPeople;
-    for (int C = 1; C < NumberOfPeople + 1; C++)
-    {
-        cout << "Enter the age of person " << C << ": ";
-        cin >> Age;
-        TotalAge = TotalAge + Age;
-    }
-    cout << "The median age is: " << ceil(TotalAge/NumberOfPeople);
+    print("hello, world")
+    
 }

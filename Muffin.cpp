30dac508aaeb95ea4a563c799b57de864976cbc4
hfmi0dzjc9z7 <thang54748646@gmail.com>
2023-12-02 03:32:31 -0800

    con me m ay beo

--- Muffin.cpp
+++ Muffin.cpp
@@ -8,21 +8,4 @@ void MedianAge();
 int main()
 {
     MedianAge();
-    return 0;
-}
-
-void MedianAge()
-{
-    int NumberOfPeople;
-    int TotalAge = 0;
-    int Age;
-    cout << "Enter the number of people: ";
-    cin >> NumberOfPeople;
-    for (int C = 1; C < NumberOfPeople + 1; C++)
-    {
-        cout << "Enter the age of person " << C << ": ";
-        cin >> Age;
-        TotalAge = TotalAge + Age;
-    }
-    cout << "The median age is: " << ceil(TotalAge/NumberOfPeople);
-}
+    
